d89cf4907a461207011822f0500b810ca761f2e0
ChangJin-Lee <ckdwls525@gmail.com>
2023-02-22 02:07:04 +0900

    Class

--- swift/practice/tempCodeRunnerFile.swift
+++ swift/practice/tempCodeRunnerFile.swift
@@ -1,15 +1,4 @@
 
-// var myName: String! = "cj"
-// var yourName: String! = nil
-
-// // if let name = myName, let friend = yourName {
-// //     print("\(name) and \(friend)")
-// // }
-// // yourName이 nil이기 때문에 실행되지 않습니다.
-
-// yourName = "haha"
-
-// if let name = myName, let friend = yourName{
-//     print("\(name) and \(friend)")
-// }
-// // cj and haha
+    // cj5.name = "cj"
+    // cj5.class = "스위프트"
+    // cj5.selfIntroduce() // 저는 스위프트반 cj입니다.
